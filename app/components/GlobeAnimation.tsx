137045bb406ca17d4e34420b5537e74d53701f82
nitturkaryash <100251269+nitturkaryash@users.noreply.github.com>
2025-01-28 16:06:57 +0530

    Replace planet ring with orbiting satellite

--- app/components/GlobeAnimation.tsx
+++ app/components/GlobeAnimation.tsx
@@ -3,15 +3,18 @@ import React from 'react';
 const GlobeAnimation = () => {
   return (
     <div className="globe-container absolute bottom-0 left-1/2 -translate-x-1/2 w-[800px] h-[800px] opacity-70">
-      {/* Rotating globe with planet ring */}
+      {/* Rotating globe */}
       <div className="globe absolute w-full h-full rounded-full animate-globe-rotate-tilted">
-        {/* Planet ring */}
-        <div className="absolute top-1/2 left-0 w-full h-[2px] transform -translate-y-1/2 -rotate-[15deg] perspective-1000">
-          <div className="absolute w-[120%] h-full -left-[10%] bg-gradient-to-r from-transparent via-blue-500/80 to-transparent ring-glow animate-flow-glow"></div>
-        </div>
-        {/* Ring shadow */}
-        <div className="absolute top-1/2 left-0 w-full h-[1px] transform -translate-y-1/2 -rotate-[15deg] opacity-30 blur-sm">
-          <div className="absolute w-full h-full bg-gradient-to-r from-transparent via-blue-900 to-transparent"></div>
+        {/* Satellite orbit path */}
+        <div className="absolute top-1/2 left-1/2 w-[120%] h-[120%] -translate-x-1/2 -translate-y-1/2 rounded-full border border-blue-500/20 -rotate-[15deg]"></div>
+        
+        {/* Satellite */}
+        <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 -rotate-[15deg]">
+          <div className="satellite w-4 h-4 animate-orbit">
+            <div className="w-full h-full bg-blue-400 rounded-full satellite-glow"></div>
+            {/* Satellite trail */}
+            <div className="absolute top-1/2 left-1/2 w-8 h-[2px] -translate-x-full -translate-y-1/2 bg-gradient-to-r from-blue-400/80 to-transparent satellite-trail"></div>
+          </div>
         </div>
       </div>
       <div className="globe-overlay absolute w-full h-3/4 top-0 bg-gradient-to-b from-gray-900 via-blue-900/50 to-transparent rounded-t-full"></div>
